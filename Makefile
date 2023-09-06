4067bd61d2c1fa3ef753d8c7a612196f6c031611
Zixuan Tan <tanzixuan.me@gmail.com>
2023-09-06 20:44:05 +0800

    add index param

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 obj-m += ptyhook.o
 
-ptyhook: ptyhook.c
+modules:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
 
-insmod: ptyhook
+insmod: modules
 	sudo insmod ptyhook.ko
 
 rmmod:
--- ptyhook.c
+++ ptyhook.c
@@ -5,67 +5,55 @@
 
 MODULE_LICENSE("GPL");
 
-static int hook_write(struct tty_struct *tty, const unsigned char *buf, int c) {
-  struct tty_driver *driver;
+// module param: index
+static unsigned int index = 0;
+module_param(index, uint, 0);
 
-  if (!c)
+static int pty_write(struct tty_struct *tty, const unsigned char *buf, int c) {
+  if (tty->flow.stopped || !c)
     return 0;
 
-  if (tty->flow.stopped)
+  if (tty->index != index)
     return 0;
 
-  driver = tty->driver;
-
-  if (driver->type != TTY_DRIVER_TYPE_PTY)
-    return 0;
-
-  if (driver->name_base == 0)
-    // don't repeat self
-    return 0;
-
-  char print_buf[512];
-  char *p = print_buf;
-  for (int i = 0; i < c && i < 100; i++) {
-    if (isprint(buf[i]))
-      *p++ = buf[i];
-    else
-      // %02x prints the hex value of the character
-      p += sprintf(p, "\\x%02x", buf[i]);
-  }
-  *p = '\0';
-
-  pr_info("hook_write: in hook_write function %s %s %d buf %d %s\n",
-          driver->driver_name, driver->name, driver->name_base, c, print_buf);
+  pr_info("pty_write: %.*s\n", c, buf);
+  dump_stack();
   return 0;
 }
 
-static int kprobes_pre(struct kprobe *t_kp, struct pt_regs *regs) {
-  hook_write(regs->di, regs->si, regs->dx);
-
+static int pre_pty_write(struct kprobe *t_kp, struct pt_regs *regs) {
+  pty_write(regs->di, regs->si, regs->dx);
   return 0;
 }
 
-/* For each probe you need to allocate a kprobe structure */
-static struct kprobe kp = {
+static struct kprobe kp_pty_write = {
     .symbol_name = "pty_write",
-    .pre_handler = kprobes_pre,
+    .pre_handler = pre_pty_write,
 };
+
+static struct kprobe *kps[] = {&kp_pty_write};
 
 static int __init kprobe_init(void) {
   int ret;
 
-  ret = register_kprobe(&kp);
+  if (index == 0) {
+    pr_err("index is 0\n");
+    return -EINVAL;
+  }
+
+  ret = register_kprobes(kps, sizeof(kps) / sizeof(kps[0]));
   if (ret < 0) {
-    pr_err("register_kprobe failed, returned % d\n", ret);
+    pr_err("register_kprobe failed, returned %d\n", ret);
     return ret;
   }
-  pr_info("Planted kprobe at % p\n", kp.addr);
+
+  pr_info("register_kprobe pass\n");
   return 0;
 }
 
 static void __exit kprobe_exit(void) {
-  unregister_kprobe(&kp);
-  pr_info("kprobe at % p unregistered\n", kp.addr);
+  unregister_kprobes(kps, sizeof(kps) / sizeof(kps[0]));
+  pr_info("unregister_kprobe pass\n");
 }
 
 module_init(kprobe_init);
